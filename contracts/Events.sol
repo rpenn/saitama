33e9658ffb61cf0d549b45896e67b56b6b8931fb
ryan <penn@ryans-MacBook-Pro.local>
2023-07-17 04:34:38 -0400

    added  function

--- contracts/Events.sol
+++ contracts/Events.sol
@@ -6,7 +6,7 @@ contract Events {
 
     mapping(uint => Event) private events;
 
-    uint public eventIdCounter;
+    uint public currentEventId;
 
     struct Event {
         address creator;
@@ -30,6 +30,16 @@ contract Events {
         // can add other event criteria as needed.
     );
 
+    event EditEvent(
+        uint indexed eventId,
+        address indexed creator,
+        string indexed location,
+        uint date,
+        uint startTime,
+        uint price
+        // can add other event criteria as needed.
+    );
+
     constructor() {
         owner = msg.sender;
     }
@@ -43,9 +53,8 @@ contract Events {
         uint eventType,
         uint price,
         uint ticketQuantity
-    ) external returns (uint currentEventId) {
-        eventIdCounter += 1;
-        currentEventId = eventIdCounter;
+    ) external {
+        currentEventId += 1;
 
         events[currentEventId] = Event({
             creator: msg.sender,
@@ -66,8 +75,65 @@ contract Events {
             date,
             startTime,
             price
+        );        
+    }
+
+    function editEvent(
+        uint eventId,
+        string memory name,
+        string memory location,
+        uint date,
+        uint startTime,
+        string memory description,
+        uint eventType,
+        // is it feasible to edit some of this stuff, post even creation? suppose tix are sold arleady?
+        uint price,
+        uint ticketQuantity
+    ) external {
+        require(eventId <= currentEventId, "Event does not exist");
+        require(events[eventId].creator == msg.sender, "Only the event creator can edit the event");
+
+        Event storage eventToUpdate = events[eventId];
+
+        if (bytes(name).length > 0 && keccak256(bytes(name)) != keccak256(bytes(eventToUpdate.name))) {
+            eventToUpdate.name = name;
+        }
+
+        if (bytes(location).length > 0 && keccak256(bytes(location)) != keccak256(bytes(eventToUpdate.location))) {
+            eventToUpdate.location = location;
+        }
+
+        if (date != eventToUpdate.date) {
+            eventToUpdate.date = date;
+        }
+
+        if (startTime != eventToUpdate.startTime) {
+            eventToUpdate.startTime = startTime;
+        }
+
+        if (bytes(description).length > 0 && keccak256(bytes(description)) != keccak256(bytes(eventToUpdate.description))) {
+            eventToUpdate.description = description;
+        }
+
+        if (eventType != eventToUpdate.eventType) {
+            eventToUpdate.eventType = eventType;
+        }
+
+        if (price != eventToUpdate.price) {
+            eventToUpdate.price = price;
+        }
+
+        if (ticketQuantity != eventToUpdate.ticketQuantity) {
+            eventToUpdate.ticketQuantity = ticketQuantity;
+        }
+
+        emit EditEvent(
+            eventId,
+            msg.sender,
+            eventToUpdate.location,
+            eventToUpdate.date,
+            eventToUpdate.startTime,
+            eventToUpdate.price
         );
-        
-        return currentEventId;
     }
 }
