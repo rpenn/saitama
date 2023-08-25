8d1dee046e5d10410e4d8a7f5384ee6edf23664c
ryan <penn@ryans-mbp.myfiosgateway.com>
2023-08-25 07:28:12 -0400

    move vite config & files to root directory saitama

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import { createBrowserRouter, RouterProvider, Route,} from "react-router-dom";
 import './index.css';
 import Root from './pages/Root';
@@ -31,7 +30,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
