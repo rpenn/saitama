b73e1d195e23d1d4d4b74386300bcd2658017eef
McGrath Coutinho <mcgrathcoutinho1@gmail.com>
2023-09-16 15:26:04 +0530

    Add tests for editEvent() function

--- test/testEventManager.sol
+++ test/testEventManager.sol
@@ -9,7 +9,8 @@ contract testEventManager is Test {
     //Stores the event manager instance
     EventManager eventManager;
 
-    //Sample event struct copy since structs ain't imported, explicit convesion required wherever used
+    //Sample addresses
+    address constant USER1 = address(1);
 
     //Deploys an EventManager contract
     function setUp() external {
@@ -46,5 +47,90 @@ contract testEventManager is Test {
         assertEq(currentEvent.eventType, 1);
         assertEq(currentEvent.price, 10 ** 17);
         assertEq(currentEvent.ticketQuantity, 50000);
+    }
+
+    function testRevertIfEventIdIsLessThanCurrentEventId() public {
+        testCreateEvent();
+        vm.expectRevert("Event does not exist");
+        eventManager.editEvent(2, "F1", "Japan Suzuka", 100, 200, 1000, "Max wins the championship", 1, 10**17, 50000);
+    }
+
+    function testRevertWhenMsgSenderIsNotCreator() public {
+        testCreateEvent();
+        vm.prank(USER1);
+        vm.expectRevert("Only the event creator can edit the event");
+        eventManager.editEvent(1, "F1", "Japan Suzuka", 100, 200, 1000, "Max wins the championship", 1, 10**17, 50000);
+    }
+
+    function testEventNameIsEditedCorrectly() public {
+        testCreateEvent();
+        eventManager.editEvent(1, "F2", "Japan Suzuka", 100, 200, 1000, "Max wins the championship", 1, 10**17, 50000);
+        uint256 currentEventId = eventManager.getCurrentEventId();
+        EventManager.Event memory currentEvent = eventManager.getEvent(currentEventId);
+        assertEq(currentEvent.name, "F2");
+    }
+
+    function testEventLocationIsEditedCorrectly() public {
+        testCreateEvent();
+        eventManager.editEvent(1, "F1", "Singapore GP", 100, 200, 1000, "Max wins the championship", 1, 10**17, 50000);
+        uint256 currentEventId = eventManager.getCurrentEventId();
+        EventManager.Event memory currentEvent = eventManager.getEvent(currentEventId);
+        assertEq(currentEvent.location, "Singapore GP");
+    }
+
+    function testEventDateIsEditedCorrectly() public {
+        testCreateEvent();
+        eventManager.editEvent(1, "F1", "Japan Suzuka", 150, 200, 1000, "Max wins the championship", 1, 10**17, 50000);
+        uint256 currentEventId = eventManager.getCurrentEventId();
+        EventManager.Event memory currentEvent = eventManager.getEvent(currentEventId);
+        assertEq(currentEvent.date, 150);
+    }
+
+    function testEventStartTimeIsEditedCorrectly() public {
+        testCreateEvent();
+        eventManager.editEvent(1, "F1", "Japan Suzuka", 100, 250, 1000, "Max wins the championship", 1, 10**17, 50000);
+        uint256 currentEventId = eventManager.getCurrentEventId();
+        EventManager.Event memory currentEvent = eventManager.getEvent(currentEventId);
+        assertEq(currentEvent.startTime, 250);
+    }
+
+    function testEventEndTimeIsEditedCorrectly() public {
+        testCreateEvent();
+        eventManager.editEvent(1, "F1", "Japan Suzuka", 100, 200, 1500, "Max wins the championship", 1, 10**17, 50000);
+        uint256 currentEventId = eventManager.getCurrentEventId();
+        EventManager.Event memory currentEvent = eventManager.getEvent(currentEventId);
+        assertEq(currentEvent.endTime, 1500);
+    }
+
+    function testEventDescriptionIsEditedCorrectly() public {
+        testCreateEvent();
+        eventManager.editEvent(1, "F1", "Japan Suzuka", 100, 200, 1500, "Ferrari win the championship?", 1, 10**17, 50000);
+        uint256 currentEventId = eventManager.getCurrentEventId();
+        EventManager.Event memory currentEvent = eventManager.getEvent(currentEventId);
+        assertEq(currentEvent.description, "Ferrari win the championship?");
+    }
+
+    function testEventEventTypeIsEditedCorrectly() public {
+        testCreateEvent();
+        eventManager.editEvent(1, "F1", "Japan Suzuka", 100, 200, 1000, "Max wins the championship", 2, 10**17, 50000);
+        uint256 currentEventId = eventManager.getCurrentEventId();
+        EventManager.Event memory currentEvent = eventManager.getEvent(currentEventId);
+        assertEq(currentEvent.eventType, 2);
+    }
+
+    function testEventPriceIsEditedCorrectly() public {
+        testCreateEvent();
+        eventManager.editEvent(1, "F1", "Japan Suzuka", 100, 200, 1000, "Max wins the championship", 1, 10**18, 50000);
+        uint256 currentEventId = eventManager.getCurrentEventId();
+        EventManager.Event memory currentEvent = eventManager.getEvent(currentEventId);
+        assertEq(currentEvent.price, 10**18);
+    }
+
+    function testEventTicketQuantityIsEditedCorrectly() public {
+        testCreateEvent();
+        eventManager.editEvent(1, "F1", "Japan Suzuka", 100, 200, 1000, "Max wins the championship", 1, 10**17, 60000);
+        uint256 currentEventId = eventManager.getCurrentEventId();
+        EventManager.Event memory currentEvent = eventManager.getEvent(currentEventId);
+        assertEq(currentEvent.ticketQuantity, 60000);
     }
 }
